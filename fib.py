d1c080f1d1afb8ecf313aece9f1036a556c17044
shoji-shoji <kotaro45hf@icloud.com>
2022-09-06 15:20:32 +0900

    フィボナッチ関数

---
+++ fib.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if n<1:
+        return 0
+    elif n==1:
+        return 1
+    elif n==1:
+        return 2
+    else:
+        return fib(n-2)+fib(n-1)
+
+for i in range(7):
+    print(fib(i))
